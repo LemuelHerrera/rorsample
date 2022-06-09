91dcd61b895bd3d14c7d66cb7b4a59be6bca6ab4
unknown <lemuelherra@gmail.com>
2022-06-09 16:06:15 +0800

    added books scaffold

--- app/controllers/org_companies_controller.rb
+++ app/controllers/org_companies_controller.rb
@@ -1,5 +1,8 @@
 class OrgCompaniesController < ApplicationController
 	def index
+		@my_hash = {a:1, b:2, c:3, d:4}
+		@hasD = @my_hash[:d]
+
 
 	end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_03_075949) do
+ActiveRecord::Schema.define(version: 2022_06_09_075317) do
+
+  create_table "books", charset: "utf8mb4", force: :cascade do |t|
+    t.string "title"
+    t.string "author"
+    t.integer "pulication_year"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "org_companies", charset: "utf8mb4", force: :cascade do |t|
     t.string "name", null: false
@@ -19,6 +27,18 @@ ActiveRecord::Schema.define(version: 2022_06_03_075949) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["typ_company_id"], name: "index_org_companies_on_typ_company_id"
+  end
+
+  create_table "org_people", charset: "utf8mb4", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_org_people_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_org_people_on_reset_password_token", unique: true
   end
 
 end
